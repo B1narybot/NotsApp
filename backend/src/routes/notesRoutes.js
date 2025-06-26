5eccd8832497c4fb896198100e57293cfbbe5403
Tounga  Saidou <123445919+B1narybot@users.noreply.github.com>
2025-06-26 10:13:52 +0200

    Notes Controllers & Db configuration
    * Projectsetup And Db
    
    * .
    
    ---------
    
    Co-authored-by: Tounga Saidou <tounga@brightgroupltd.com>

---
+++ backend/src/routes/notesRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { createNote, DeleteNote, getAllNotes, UpdateNote } from "../controllers/notesControllers.js";
+
+const router = express.Router();
+
+router.get("/", getAllNotes); 
+router.post("/", createNote); 
+ router.put("/:id", UpdateNote); 
+ router.delete("/:id", DeleteNote); 
+
+export default router; 
